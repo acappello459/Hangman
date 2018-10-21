3efccb3ea3fd8f791135599cb8e02c4da3aa36e5
acappello459 <acappello459@outlook.com>
2018-10-21 14:46:17 -0400

    added mobile responsiveness

--- index.js
+++ index.js
@@ -4,9 +4,11 @@
 var body = document.getElementsByTagName('body')[0];
 var input = document.getElementById('input');
 var enter = document.getElementById('enter');
-var usedLetter = document.getElementById('usedLetters');
+var wrongLetter = document.getElementById('wrongLetters');
 var newGame = document.getElementById('newGame');
 var picture = document.getElementById('picture');
+var hintButton = document.getElementById('hintButton')
+var guessWord = document.getElementById('guessWord')
 var hint = document.getElementById('hint')
 
 // window.addEventListener('load', alert('Would you like to play a game'))
@@ -60,7 +62,8 @@ a.addWords(word10)
 var randomNumber = Math.floor(Math.random()*a.wordArray.length);
 var randomWord = a.wordArray[randomNumber].word;
 var randomHint = a.wordArray[randomNumber].hint;
-var usedLetters = [];
+var wrongLetters = [];
+var rightLetters = [];
 var remainingLetters = randomWord.length;
 var counter = 0;
 // var images = [hangman0.png, hangman1.png, hangman2.png, hangman3.png, hangman4.png, hangman5.png, hangman6.png ]
@@ -75,15 +78,18 @@ function startGame(){
   for(let i=0; i<randomWord.length;i++){
     var brick = document.createElement('div')
     brick.id = 'brick' + [i]
-    brick.style.width = '100px';
+    brick.style.width = '7.5%';
     brick.style.height = '100px';
     brick.style.border = "2px solid"
     brick.style.display = "inline-block";
     brick.style.margin = '5px 5px'
+    // brick.style.padding = '50px'
     brick.style.overflow = 'hidden'
-    body.appendChild(brick)
+    brick.style.textAlign = 'center'
+    guessWord.appendChild(brick)
 
   if(randomWord[i] == ' '){
+    brick.style.display = "block";
     brick.style.border = ''
     remainingLetters--;
   }
@@ -92,52 +98,106 @@ function startGame(){
 
 function checkLetter(){
   var result = false;
-  if(input.value.length != 1){
+  if(input.value.length > 1){
     alert("You can only enter one letter at a time you dingus!")
     result = true;
+  }else if(input.value.length == 0){
+    alert("You didnt enter in a letter you dingus!")
   }else{
-    console.log(input.value)
+    // console.log(input.value)
   for(let i=0; i<randomWord.length;i++){
     if(input.value.toUpperCase() == randomWord[i]){
       document.getElementById('brick'+[i]).innerHTML = randomWord[i]
-      document.getElementById('brick'+[i]).style.fontSize = '2em'
+      document.getElementById('brick'+[i]).style.fontSize = '2vh'
       document.getElementById('brick'+[i]).style.textAlign = 'center'
-      remainingLetters--;
+      document.getElementById('brick'+[i]).style.backgroundColor = 'lightGreen'
+      if(rightLetters.includes(input.value) == false){
+        remainingLetters--;
+      }
       result = true;
       // console.log(remainingLetters)
-      // document.getElementById('brick'+[i]).style.padding = '10px auto'
+
         }
       }
     }
-      if(result == false){
-        counter++;
-        picture.style.backgroundImage = "url("+"stylesheets/images/hangman"+[counter]+".png"+")"
-        console.log(counter);
+    if(result == true){
+      if(rightLetters.includes(input.value)){
+        alert("You've already guessed that letter you dingus!")
+        // remainingLetters++;
+      }else{
+        rightLetters.push(input.value)
       }
+    }
+      //
+      // if(result == false){
+      //   counter++;
+      //   console.log(counter);
+      // }
     if(remainingLetters === 0){
-      // document.getElementById('brick'+[i]).innerHTML = randomWord[i];
-      let youWin = confirm("Congratulations you won! If you would like to play again please hit OK!");
-      if(youWin == true){
-        document.location.reload()
-        startGame()
-      }
+      setTimeout(function(){
+        if(confirm("Congratulations you guessed "+[randomWord]+" correctly! If you would like to play again please hit OK!")){
+          location.reload();
+        }
+      }, 500);
   }
 
-  if(input.value.length == 1){
-  usedLetters.push(input.value)
-  usedLetter.innerHTML += input.value
+    if(result==false && input.value.length == 1){
+      console.log(counter)
+      if(wrongLetters.includes(input.value)){
+        alert("You've already guessed that letter you dingus!")
+      }else{
+        counter++
+        wrongLetters.push(input.value)
+        wrongLetter.innerHTML = wrongLetters
+        wrongLetter.style.textAlign = 'center'
+        picture.style.backgroundImage = "url("+"stylesheets/images/hangman"+[counter]+".png"+")"
+      }
+  }if(counter == 6){
+    picture.style.backgroundImage = "url("+"stylesheets/images/hangman6.png"+")"
+    setTimeout(function(){
+      if(confirm("BOOOOOOOOOOOOO!"+" You couldn't even guess " + [randomWord]+" correctly!"+ " If you would like to play again please hit OK!")){
+        location.reload()
+      }
+    }, 500);
+
+
 }
 }
 
 
-newGame.addEventListener('click', startGame);
-newGame.addEventListener('click',function display(){
-  newGame.style.display = 'none'
-})
 
+//event listener that enters the guessed letter on the push of enter
+input.addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        enter.click();
+        input.value = "";
+        function enterTheLetter(event) {
+          checkLetter();
 
+    }
+}
+});
+//event listener that enters the guessed letter if the user clicks the button instead of pressing enter
 enter.addEventListener('click', checkLetter)
 enter.addEventListener('click', function(){
   console.log('butt')
   input.value = ''
+})
+
+hintButton.addEventListener('click', function(){
+ // var hint = document.createElement('div');
+ hint.value = ''
+ hint.id = 'hint';
+ hint.style.height = '60px';
+ hint.style.fontSize= '3vh';
+ hint.style.color = 'white';
+ hint.innerHTML = "Hint: " +randomHint;
+ guessWord.appendChild(hint)
+})
+
+//event listener that triggers the start game function on click, then hides the button so you can't duplicate the game
+newGame.addEventListener('click', startGame);
+newGame.addEventListener('click',function display(){
+  newGame.style.display = 'none'
 })
